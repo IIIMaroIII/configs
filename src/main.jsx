6843163fb0c1aed45ec2d34d0296f7e1363c7d67
Rodion Matvieiev <matveev.r.v@icloud.com>
2024-04-07 18:05:53 -0400

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+
 import 'modern-normalize/modern-normalize.css';
 import 'the-new-css-reset/css/reset.css';
-
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
